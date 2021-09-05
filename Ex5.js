8e593e2878cf26562223a461b8566e7077ad8263
Tanya <tanya.campos898@gmail.com>
2021-09-05 20:49:24 +0800

    Ex5 minor edits

--- Ex5.js
+++ Ex5.js
@@ -1,13 +1,6 @@
-
-let x = 0;
-if (x>0) {
-  console.log("hey")
-} else {
-  console.log("hi");
-};
-
+//Exercise 5
 let spend1 = 400; //entertainment spend
-let spend2 = 150; //eating out spend
+let spend2 = 350; //eating out spend
 let spend3 = 500; //transport spend
 
 
@@ -16,12 +9,9 @@ if (spend1 > 500) {
 } else if (spend1 > 300) {
   if (spend2 > 250) {
     alert("Entertainment spend exceeds 300 and eating-out spend exceeds 250!")
-  } else {
-    if (spend3 > 400) {
+  } else if (spend3 > 400) {
       alert("Entertainment spend exceeds 300 and transport spend exceeds 400!")
     }
-  }
-} /*else {
-  return
-};*/
-// is it ok if there's no final else statement?
+} else {
+  alert("Spending within budget.")
+};
