cf3c7e904d6d09b21011968b0b595b29a0e19634
Tanya <tanya.campos898@gmail.com>
2021-09-05 15:23:20 +0800

    Updated Ex6 to include a shorter version

--- Ex6.js
+++ Ex6.js
@@ -1,8 +1,8 @@
 //Exercise 6: forEach loop, add 2% only for amounts amove 4000, store value in same array
 var exp = [4500, 5000, 6500, 5000, 2800, 3500];
 
-//defining the function
 //update the function to add 2% to expenses above 4,000
+//declaring a function then using it as a callback to foreach
 let interestFxn = function(num, index, exp) {
   if (num > 4000) {
     num = (num*1.02)
@@ -12,12 +12,25 @@ let interestFxn = function(num, index, exp) {
   exp[index] = num;
   
 };
-//using forEach
-exp.forEach(interestFxn, exp);
+
+exp.forEach(interestFxn);
 console.log(exp);
 
+//shorter version of code above
+var expense = [4500, 5000, 6500, 5000, 2800, 3500];
+let interestFn = function(num, index, expense) {
+  if (num > 4000) {
+  
+  expense[index] = num*1.02;}
+  
+};
+
+expense.forEach(interestFn);
+console.log(expense)
+
+
 //Using inline callback function
-/*var expenses = [4500, 5000, 6500, 5000, 2800, 3500];
+var expenses = [4500, 5000, 6500, 5000, 2800, 3500];
 expenses.forEach(function(num, index, array) {
   if (num > 4000) {
     num = (num*1.02);
@@ -25,4 +38,4 @@ expenses.forEach(function(num, index, array) {
   }})
 
 console.log(expenses)
-*/
+
